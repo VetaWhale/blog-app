d4bac359ebbdb2f06f818bdb6940e7dc894a3129
Elizaveta Shkimbova <veta-whale@yandex.ru>
2023-05-26 18:35:16 +0300

    added app

--- index.js
+++ index.js
@@ -1,76 +1,114 @@
-// объявляем переменные из html
-const postTitleInputNode = document.querySelector('.js-title-post');
-const postTextInputNode = document.querySelector('.js-text-post');
-const newPostBtnNode = document.querySelector('.js-send-btn');
-const postsNode = document.querySelector('.js-posts');
+const STANDART_CONTENT_TITLE = 'Рекомендуемая длина заголовка: 100 символов';
+const STANDART_CONTENT_TEXT = 'Рекомендуемая длина поста: 200 символов';
+const VALUE_TITLE_OUTSIDE = 'Заголовок больше 100 символов';
+const VALUE_TEXT_OUTSIDE = 'Пост больше 200 символов';
+// достаем все нужные нам элементы в html файлаи записываем их в константы
+const inputTitle = document.querySelector('.js-title-post');
+const inputText = document.querySelector('.js-text-post');
+const buttonAddPost = document.querySelector('.js-send-btn');
+const historyList = document.querySelector('.js-posts');
+const inputOutsideTitle = document.querySelector('.js-expenses-symbol-title');
+const inputOutsideText = document.querySelector('.js-expenses-symbol-text');
 
-// объявляем объект в котором описана структура нашего содержания
-// let post = {
-//     title: '',
-//     text: '',
-// };
-// перезаписываем наш объект на массив, который будет содержать объекты
 const posts = [];
 
-// обработчик события по клику
-newPostBtnNode.addEventListener('click', function() {
-    const postFromUser = getPostFromUser();
+inputTitle.addEventListener('keyup', function() {
+    getTitleFromUser();
+})
+
+inputText.addEventListener('keyup', function() {
+    getTextFromUser();
+})
+
+buttonAddPost.addEventListener('click', function() {
+    const lengthTitle = getTitleFromUser();
+    const lengthText = getTextFromUser();
+
+    if (lengthTitle > 100 || lengthText > 200) return;
+
+    clearOutsideText();
+
+    const postFromUser = getValueFromUser();
 
     addPost(postFromUser);
 
     renderPosts();
 })
 
-// функция получения данных от пользователя
-function getPostFromUser() {
-    // переменные, которые получают значения из полей ввода
+function getTitleFromUser() {
+    let lengthTitle = inputTitle.value.length;
+
+    if (lengthTitle > 100) {
+        inputOutsideTitle.innerText = VALUE_TITLE_OUTSIDE;
+        inputOutsideTitle.classList.add('red');
+    } else {
+        inputOutsideTitle.innerText = STANDART_CONTENT_TITLE;
+        inputOutsideTitle.classList.remove('red');
+    }    
+    return lengthTitle;
+}
+function getTextFromUser() {
+    let lengthText = inputText.value.length;
+
+    if (lengthText > 200) {
+       inputOutsideText.innerText = VALUE_TEXT_OUTSIDE; 
+       inputOutsideText.classList.add('red');
+    } else {
+        inputOutsideText.innerText = STANDART_CONTENT_TEXT;
+        inputOutsideText.classList.remove('red');
+    }
+    return lengthText;
+}
+
+function clearOutsideText() {
+    inputOutsideTitle.innerText = '';
+    inputOutsideText.innerText = '';
+}
+
+function getValueFromUser() {
     const today = new Date();
-    let date = today.toLocaleString();
-    console.log(date);
-    const title = postTitleInputNode.value;
-    const text = postTextInputNode.value;
-    // возвращение из функции в структуру объекта
+    const date = today.toLocaleString();
+    const title = inputTitle.value;
+    const text = inputText.value;
+
+    if (!title || !text) return;
+
+    clearInput();
+
     return {
         date: date,
         title: title,
         text: text,
-    };
+    }; 
 }
-
-// функция возращения 
-// перезаписываем на добавление поста, чтобы в массив записывался пост, в параметры записываем ключи из объекта
+function clearInput() {
+    inputTitle.value = '';
+    inputText.value = '';
+}
 function addPost({ date, title, text}) {
-    // прописываем структуру, которая будет записываться в массив
     posts.push({
         date,
         title,
-        text
+        text,
     })
 }
-
-// функция возращения объекта, для дальнейшего к нему обращения
 function getPosts() {
     return posts;
 }
-
-// функция размещения содержания поста в структуру html
 function renderPosts() {
-    // объявляем переменную, чтобы внутри фукнции обратиться к объекту снаружи
     const posts = getPosts();
 
-    // объявляем переменную для записи туда содержания
     let postsHTML = '';
 
     posts.forEach(post => {
-       postsHTML += `
-    <div class="post">
-        <p class="post__date">${post.date}</p>
-        <p class="post__title">${post.title}</p>
-        <p class="post__text">${post.text}</p>
-    </div>
+        postsHTML += `
+        <div class='post'>
+            <p class='post__date'>${post.date}</p>
+            <p class='post__title'>${post.title}</p>
+            <p class='post__text'>${post.text}</p>
+        </div>
     `;
     });
 
-    // меняем структуру html в post
-    postsNode.innerHTML = postsHTML;
+    historyList.innerHTML = postsHTML;
 }
